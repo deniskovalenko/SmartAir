02cc5c90be1f85fb2d7440ff7a8e184a85e919bd
Amira Sheremet <amira_sheremet@gmail.com>
2015-04-11 23:21:05 +0300

    UserModel : simplify (deleted city, country, name and surname fields)

--- src/main/java/com/breathe/model/UserModel.java
+++ src/main/java/com/breathe/model/UserModel.java
@@ -9,10 +9,6 @@ public class UserModel {
     private String username;
     private String email;
     private String password;
-    private String name;
-    private String surname;
-    private String country;
-    private String city;
     private List<DeviceModel> devices;
 
     public String getUsername() {
@@ -37,38 +33,6 @@ public class UserModel {
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
     }
 
     public List<DeviceModel> getDevices() {
