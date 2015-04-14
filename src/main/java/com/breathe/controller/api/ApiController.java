1e2349ec2beb2f33f5f651906a4699ffc0ae46b7
denis <denis.v.kovalenko@gmail.com>
2015-04-14 23:56:42 +0300

    fixing dependencies

--- src/main/java/com/breathe/controller/api/ApiController.java
+++ src/main/java/com/breathe/controller/api/ApiController.java
@@ -5,16 +5,9 @@ import com.breathe.model.api.ApiDeviceModel;
 import com.breathe.model.api.ApiResponseModel;
 import com.breathe.service.ApiService;
 import com.breathe.service.StatisticService;
-import com.breathe.service.UserService;
-import com.mongodb.DB;
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
-import com.sun.xml.internal.ws.client.ResponseContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
-import java.net.UnknownHostException;
 import java.util.Date;
 import java.util.List;
 
@@ -25,16 +18,9 @@ import java.util.List;
 @RequestMapping("/api")
 public class ApiController {
     @Autowired
-    private UserService userService;
-    @Autowired
     private StatisticService statisticService;
     @Autowired
     private ApiService apiService;
-
-    public ApiController() throws UnknownHostException {
-        final MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://localhost"));
-        final DB co2Database = mongoClient.getDB("co2");
-    }
 
     @RequestMapping(value = "/statistic", method = RequestMethod.GET)
     public @ResponseBody
