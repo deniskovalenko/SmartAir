959f16f5111a3c23adc01711e25830ebd7a5156b
Amira Sheremet <amira_sheremet@gmail.com>
2015-04-18 22:15:52 +0300

    added method addDevice to user

--- src/main/java/com/breathe/dao/DeviceDAO.java
+++ src/main/java/com/breathe/dao/DeviceDAO.java
@@ -12,4 +12,6 @@ public interface DeviceDAO {
     boolean ifDeviceExists(String deviceId);
 
     boolean addDevice(String deviceId, String deviceName, int delay, int co2MinLevel);
+
+    boolean addDevice(String userId, String deviceId, String deviceName, int delay, int co2MinLevel);
 }
--- src/main/java/com/breathe/dao/implementation/DeviceDAOImpl.java
+++ src/main/java/com/breathe/dao/implementation/DeviceDAOImpl.java
@@ -12,6 +12,7 @@ import java.net.UnknownHostException;
 @Repository
 public class DeviceDAOImpl implements DeviceDAO {
     DBCollection deviceCollection; //collection of manufactured, but maybe not purchased yet devices
+    DBCollection usersCollection;
     MongoClient mongoClient;
     DB co2Database;
 
@@ -19,6 +20,7 @@ public class DeviceDAOImpl implements DeviceDAO {
         mongoClient= new MongoClient(new MongoClientURI("mongodb://localhost"));
         co2Database = mongoClient.getDB("co2");
         deviceCollection = co2Database.getCollection("data");
+        usersCollection = co2Database.getCollection("users");
     }
 
     public DBObject findByDeviceId(String deviceId) {
@@ -42,10 +44,35 @@ public class DeviceDAOImpl implements DeviceDAO {
         BasicDBObject post = new BasicDBObject("deviceId", deviceId)
             .append("deviceName", deviceName)
             .append("delay", delay)
-            .append("co2MinLevel", co2MinLevel);
+            .append("co2Min", co2MinLevel);
 
         try {
             deviceCollection.insert(post);
+        } catch (Exception e) {
+            System.out.println("Error inserting post");
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean addDevice(String userId, String deviceId, String deviceName, int delay, int co2MinLevel) {
+        if (deviceCollection.find(new BasicDBObject("deviceId", deviceId)).count() > 0) {
+            System.out.println("Device with this device_id already exists: " + deviceId);
+            return false;
+        }
+        if (usersCollection.find(new BasicDBObject("_id", userId)).count() == 0) {
+            System.out.println("User with this _id doesn't exist: " + userId);
+            return false;
+        }
+
+        DBObject find = new BasicDBObject("_id", userId);
+        DBObject push = new BasicDBObject("devices", new BasicDBObject("deviceId", deviceId)
+                .append("deviceName", deviceName)
+                .append("delay", delay)
+                .append("co2Min", co2MinLevel));
+        try {
+            usersCollection.update(find, new BasicDBObject("$push", push));
         } catch (Exception e) {
             System.out.println("Error inserting post");
             return false;
